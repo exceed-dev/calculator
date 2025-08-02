33110d9a997e2932dc0ae655931e2451b8723599
Bagas Setia <bagas23setia@gmail.com>
2025-08-02 11:21:26 +0700

    Add click event for number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 const mainScreen = document.querySelector("#main-screen");
+const numberButtons = document.querySelectorAll("[data-number]");
 
+// click event
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    handleInputNumber(button.textContent);
+  });
+});
+
+// function
 function handleInputNumber(number) {
   if (mainScreen.textContent === "0") {
     mainScreen.textContent = number;
