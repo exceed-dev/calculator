a95a6a0907100f965982a8bc920bb91f004d8e00
Bagas Setia <bagas23setia@gmail.com>
2025-08-02 11:33:40 +0700

    Add handleInputOperator function and operator event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
+let firstOperand = "";
+let operator = null;
+
 const mainScreen = document.querySelector("#main-screen");
+const sencondaryScreen = document.querySelector("#secondary-screen");
 const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
 
 // click event
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
     handleInputNumber(button.textContent);
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    handleInputOperator(button.textContent);
   });
 });
 
@@ -15,4 +26,11 @@ function handleInputNumber(number) {
   } else {
     mainScreen.textContent += number;
   }
+}
+
+function handleInputOperator(op) {
+  firstOperand = mainScreen.textContent;
+  operator = op;
+  sencondaryScreen.textContent = `${firstOperand} ${operator}`;
+  mainScreen.textContent = "0";
 }
